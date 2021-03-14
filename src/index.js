fd867283da8c40ba72153a00f45fff174d8ce76b
Vadavur <vadavur@gmail.com>
2021-03-14 13:47:34 +0300

    feat: realize check function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,29 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  
+  // make the array of brackets from the string
+  
+  strArr = str.split('');
+
+  // make the map of brackets (key - opening bracket, value - closing bracket)
+  
+  const bracketsMap = new Map();
+  bracketsConfig.forEach((element) => {bracketsMap.set(element[0], element[1])});
+
+  // go through the array
+  // removing close standing pairs of the same opening-closing brackets
+  // jumping back, if pair was removed
+
+  for (let i = 0; i < strArr.length;){
+    if ((strArr[i + 1] != undefined) && (strArr[i + 1] == bracketsMap.get(strArr[i]))){
+      strArr.splice(i, 2);
+      if (i != 0) {i--} ;
+    } else i++;
+  }
+
+  // return true if all brackets were removed
+
+  if (strArr.length == 0){
+    return true;
+  }
+  else return false;
 }
